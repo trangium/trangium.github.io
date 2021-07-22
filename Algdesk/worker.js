f5da6d98ee3a8b1556c85b6ed8f44c71cd90007b
trangium <vincent.k.trang@gmail.com>
2021-07-22 18:37:30 -0400

    Added batch solving!

--- Algdesk/worker.js
+++ Algdesk/worker.js
@@ -1,19 +1,34 @@
-let subPuzzle;
-let fullPuzzle;
-let input;
-
 self.onmessage = function (msg) {
-    let inputs = msg.data;
-    for (let i=0; i<inputs.length; i++) {
-        input = inputs[i];
-        calc();
-        postMessage({value: null, type: "reset-depth"})
+    input = msg.data;
+    let [fullPuzzle, subPuzzles] = setPuzzles(input.puzzle, input.ignore, input.subgroups); // loses efficiency if puzzle has not changed, so update this
+
+    let solutionIndex = 1;
+    for (let stateStr of fullPuzzle.getBatchStates(input.solve)) {
+        let state = fullPuzzle.execute(fullPuzzle.solved, fullPuzzle.moveStrToList(stateStr));
+        if(!(arraysEqual(fullPuzzle.solved, state))) {
+            postMessage({value: solutionIndex, type: "next-state"})
+            calcState(state, subPuzzles);
+            solutionIndex++;
+        }
     }
     postMessage({value: null, type: "stop"});
 };
 
-function setPuzzle() {
-    let moves = input.puzzle;
+function calcState(state, subPuzzles) {
+    for (let subData of subPuzzles) {
+        for (let solution of subData.puzzle.solve(state, isNaN(parseInt(subData.search))?Infinity:parseInt(subData.search))) {
+            postMessage({value: subData.puzzle.moveListToStr(solution), type: "solution"});
+        }
+        postMessage({value: null, type: "reset-depth"})
+    }
+}
+
+function removeBrackets(s) { // Removes (), {}, <>, and []
+    return s.replace(/\(|\)|\[|\]|{|}|<|>/g, "");
+}
+
+function setPuzzles(puzzleDef, ignore, subgroups) {
+    let moves = puzzleDef;
     let moveLines = moves.split('\n');
 
     let pieceList = [];
@@ -84,7 +99,7 @@ function setPuzzle() {
     }
 
     // Deal with equivalences
-    let splitEquivalences = input.ignore.split('{');
+    let splitEquivalences = ignore.split('{');
     let solvedState = [];
     for (let i=0; i<pieceList.length; i++) {solvedState[i] = i}
     for (let i=1; i<splitEquivalences.length; i++) {
@@ -95,23 +110,21 @@ function setPuzzle() {
             solvedState[pieceList.indexOf(equivPieces[j])] = equivNum;
         }
     }
-    subPuzzle = new Puzzle(cubeOri.slice(), moveList.slice(), clockwiseMoveStr.slice(), solvedState.slice());
-    fullPuzzle = new Puzzle(cubeOri.slice(), moveList.slice(), clockwiseMoveStr.slice(), solvedState.slice());
-    return pieceList;
+
+    let fullPuzzle = new Puzzle(cubeOri.slice(), moveList.slice(), clockwiseMoveStr.slice(), solvedState.slice());
+    let subPuzzles = [];
+    for (let sub of subgroups) {
+        subPuzzles.push({puzzle: getSubPuzzle(pieceList, fullPuzzle, ignore, sub.subgroup, sub.prune), search: sub.search});
+    }
+
+    initCubeOri(fullPuzzle, pieceList, ignore);
+    fullPuzzle.setAdjustMoves(fullPuzzle.moveStrToList("U"));
+    
+    return [fullPuzzle, subPuzzles];
 }
 
-function calc() {
-    function removeBrackets(s) { // Removes (), {}, <>, and []
-        return s.replace(/\(|\)|\[|\]|{|}|<|>/g, "");
-    }
-    let pieceList = setPuzzle(); // loses efficiency if puzzle has not changed, so update this
-
-    let generators = removeBrackets(input.subgroup).replace(",","").split(" ");
-    if (input.subgroup.replace(" ","").length > 0) {
-        subPuzzle = fullPuzzle.setSubgroup(generators);
-    }
-
-    let lines = input.ignore.split("\n");
+function initCubeOri(pzl, pieceList, ignore) {
+    let lines = ignore.split("\n");
     for (let i=0; i<lines.length; i++) {
         let line = lines[i];
         if (line.includes(":")) {
@@ -119,22 +132,51 @@ function calc() {
             let orientData = line.split(":")[1].split(" ");
             for (let i=0; i<orientData.length; i++) {
                 let pieceIndex = pieceList.indexOf(removeBrackets(orientData[i]));
-                if (pieceIndex !== -1) {subPuzzle.cubeOri[pieceIndex] = numOri}
+                if (pieceIndex !== -1) {pzl.cubeOri[pieceIndex] = numOri}
             }
         }
     }
+}
 
-    subPuzzle.createPrun(parseInt(input.prune));
+function getSubPuzzle(pieceList, fullPuzzle, ignore, subgroup, prune) {
+    let generators = (subgroup.replace(" ","").length > 0) ? removeBrackets(subgroup).replace(",","").split(" "): fullPuzzle.clockwiseMoveStr;
+    let subPuzzle = fullPuzzle.setSubgroup(generators);
 
-    let solve = subPuzzle.solve(fullPuzzle.execute(subPuzzle.solved, fullPuzzle.moveStrToList(removeBrackets(input.solve))), isNaN(parseInt(input.search))?Infinity:parseInt(input.search));
-    function nextSolution() {
-        let solution = solve.next();
-        while (solution.done == false) {
-            postMessage({value: subPuzzle.moveListToStr(solution.value), type: "solution"});
-            solution = solve.next();
+    initCubeOri(subPuzzle, pieceList, ignore);
+
+    subPuzzle.setAdjustMoves(subPuzzle.moveStrToList("U")); // Do AUF (TODO: Add AUF input field)
+    subPuzzle.createPrun(parseInt(prune));
+
+    return subPuzzle;
+}
+
+function parseBatch(input) {
+    let closingChar = "";
+    let dataInside = "";
+    let finalData = [];
+    const bracketMap = new Map([["[","]"],["<",">"]]);
+    for (let i=0; i<input.length; i++) {
+        let char = input[i]; 
+        if (closingChar !== "") {
+            if (char === closingChar) {
+                finalData.push([closingChar, dataInside]);
+                closingChar = ""; 
+                dataInside = "";
+            } else {
+                dataInside += char;
+            }
+        } else {
+            if (!(bracketMap.has(char))) {
+                dataInside += char;
+            }
+            if (bracketMap.has(char) || i === input.length - 1) {
+                finalData.push(["", dataInside]);
+                closingChar = bracketMap.get(char);
+                dataInside = "";
+            }
         }
     }
-    nextSolution();
+    return finalData;
 }
 
 // END HTML-SIDE JS
@@ -144,6 +186,13 @@ function arraysEqual(arr1,arr2) {
         if (arr1[i] !== arr2[i]) {return false}
     }
     return true
+}
+
+function cartesian(arrays) { 
+	let singleStep = (arrays, add) => {return arrays.concat(...add.map(next => arrays.map( arr => arr.concat(next) )))} 
+	let prod = [[]];
+    for (arr of arrays) {prod = singleStep(prod, arr)}
+    return prod;
 }
 
 class Puzzle {
@@ -164,6 +213,7 @@ class Puzzle {
         this.posMask = (1 << this.posBits) - 1; // 31
         this.oriMask = ((1 << Math.ceil(Math.log2(Math.max(...cubeOri)))) - 1) << this.posBits; // 3 * 32 = 96
         this.totalBits = Math.ceil(Math.log2(this.oriMask)); // 7
+        this.clockwiseMoveStr = clockwiseMoveStr.slice();
         this.moveStr = [];
 
         this.nullmove = [];
@@ -230,11 +280,27 @@ class Puzzle {
         // initialize prunetable (empty upon initialization)
         this.pruneTable = new Map();
         this.pruneDepth = 0;
+
+        // initialize adjust moves (none upon initialization)
+        this.adjustSequences = [[]];
+        this.adjustMovesTable = [];
+        for (let i=0; i<this.moves.length; i++) {this.adjustMovesTable[i] = false}
+        this.adjustCount = 0;
+    }
+
+    // executes sequence on specified starting state
+    execute(start, sequence) {
+        let tempCube = [];
+        for (let j=0; j<sequence.length; j++) {
+            this.mult(start, this.moves[sequence[j]],tempCube)
+            start = tempCube.slice();
+        }
+        return start
     }
 
     // returns next valid move given previous move. For example, U U2 is invalid, but U R is valid.
     nextValid(prevMove, move) {
-        while (move < this.pcCount) {
+        while (move < this.moves.length) {
             move += 1;
             if (this.validPairs[prevMove][move]) {
                 return move;
@@ -252,37 +318,75 @@ class Puzzle {
         return string;
     }
 
-    // advances an array of moves to the next valid array. returns the length of the array.
-    advance(arr) {
-        let moveInd = arr.length-1;
-        while (moveInd < arr.length) {
-            if (moveInd) {
-                arr[moveInd] = this.nextValid(arr[moveInd-1],arr[moveInd])
-            } else {
-                arr[0] = arr[0]+1;
-                if (arr[0] == this.moves.length) {
-                    arr[0] = 0;
-                    let newLength = arr.length+1
-                    for (let i=1; i<newLength; i++) {
-                        arr[i] = this.nextValid(arr[i-1],-1)
+    setAdjustMoves(moveList) {
+        this.adjustCount = moveList.length;
+        let adjustMoves = [];
+        for (let i=0; i<this.moves.length; i++) {this.adjustMovesTable[i] = false}
+        for (let moveNum of moveList) {
+            let moveReps = [];
+            let currentRep = moveNum;
+            this.adjustMovesTable[currentRep] = true;
+            while (true) {
+                moveReps.push(currentRep);
+                let move = this.execute(this.moves[currentRep], [moveNum]);
+                currentRep = -1;
+                for (let i=0; i<this.moves.length; i++) {
+                    if (arraysEqual(this.moves[i], move)) {
+                        currentRep = i;
+                        this.adjustMovesTable[currentRep] = true;
+                        break;
                     }
-                    postMessage({value:1, type: "depthUpdate"});
-                    return newLength
                 }
+                if (currentRep === -1) {break}
             }
-            if (arr[moveInd] == -1) {moveInd--} else {moveInd++}
+            adjustMoves.push(moveReps);
         }
-        return arr.length
+        this.adjustSequences = cartesian(adjustMoves);
     }
 
-    // executes sequence on specified starting state
-    execute(start, sequence) {
-        let tempCube = [];
-        for (let j=0; j<sequence.length; j++) {
-            this.mult(start, this.moves[sequence[j]],tempCube)
-            start = tempCube.slice();
+    // like nextValid, but with the first move of a sequence.
+    nextValidInitial(move) {
+        let x = move+1;
+        while (x < this.moves.length) {
+            if (!(this.adjustMovesTable[x])) {return x};
+            x++;
         }
-        return start
+        return -1;
+    }
+
+    // stores first valid array of moves with given length in arr
+    firstValidArray(length) {
+        let arr = [this.nextValidInitial(-1)];
+        for (let i=1; i<length; i++) {
+            arr[i] = this.nextValid(arr[i-1],-1);
+        }
+        return arr;
+    }
+
+    *getAllSequences(seqLength) {
+        if (seqLength === 0) {
+            for (let sequence of this.adjustSequences) {
+                yield sequence; // need to account for adjust moves
+            }
+            return;
+        }
+        postMessage({value: 1, type: "depthUpdate"});
+        let arr = this.firstValidArray(seqLength);
+        while (true) {
+            for (let sequence of this.adjustSequences) {
+                yield sequence.concat(arr); // need to account for adjust moves
+            }
+            let moveInd = seqLength-1;
+            while (moveInd < seqLength) {
+                if (moveInd) {
+                    arr[moveInd] = this.nextValid(arr[moveInd-1],arr[moveInd])
+                } else {
+                    arr[0] = this.nextValidInitial(arr[0]);
+                    if (arr[0] == -1) {return}
+                }
+                if (arr[moveInd] == -1) {moveInd--} else {moveInd++}
+            }
+        }
     }
 
     // convert a sequence of moves, internally represented by a string of numbers, into human-readable text
@@ -317,19 +421,18 @@ class Puzzle {
     // create a prune table
     createPrun(maxDepth) {
         let tempTable = new Map();
-        let attempt = [0];
-        let depth = 1;
-        while (depth <= maxDepth) {
-            let cubeStr = this.compressArr(this.execute(this.solved, attempt));
-            if (!(tempTable.has(cubeStr))) {tempTable.set(cubeStr, depth)}
-            depth = this.advance(attempt);
+        for (let depth=0; depth<=maxDepth; depth++) {
+            for (let sequence of this.getAllSequences(depth)) {
+                let cubeStr = this.compressArr(this.execute(this.solved, sequence));
+                if (!(tempTable.has(cubeStr))) {tempTable.set(cubeStr, sequence.length)}
+            }
         }
         this.pruneTable = tempTable;
         this.pruneDepth = maxDepth;
     }
 
     // read all solutions from a given state under the prune table's depth
-    *readPrun(state, exactDepth=false, partialSolve=[], maxDepth=this.pruneDepth) { // maxDepth should be the same as the prune table's maxDepth
+    *readPrun(state, partialSolve=[], exactDepth=false, maxDepth=this.pruneDepth) { // maxDepth should be the same as the prune table's maxDepth
         for (let m=0; m<this.moves.length; m++) {
             if (partialSolve.length == 0 || this.validPairs[partialSolve[partialSolve.length-1]][m]) {
                 let nextState = this.execute(state, [m]);
@@ -340,23 +443,30 @@ class Puzzle {
                         yield * [fullSolve];
                     }
                 } else if (nextDistance < maxDepth) { // false if nextDistance is undefined
-                    yield * this.readPrun(nextState, exactDepth, partialSolve.concat(m), maxDepth-1);
+                    yield * this.readPrun(nextState, partialSolve.concat(m), exactDepth, maxDepth-1);
                 }
             }
         }
     }
 
-    *solve(state, searchDepth) {
-        yield * this.readPrun(state);
-        let attempt = [0];
-        let depth = 1;
-        while (depth <= searchDepth) {
-            let nextState = this.execute(state, attempt);
-            if (this.pruneTable.get(this.compressArr(nextState)) <= this.pruneDepth) {
-                yield * this.readPrun(nextState, true, attempt);
-            }
-            depth = this.advance(attempt);
-        }                
+    *solve(state, searchDepth, startDepth=0) {
+        for (let depth=startDepth; depth<=searchDepth; depth++) {
+            for (let sequence of this.getAllSequences(depth)) {
+                let nextState = this.execute(state, sequence);
+                let thisDistance = this.pruneTable.get(this.compressArr(nextState));
+                if (thisDistance !== undefined) {
+                    if (sequence.length === 0) {
+                        yield * this.readPrun(nextState, sequence, false);
+                    } else {
+                        let prevState = this.execute(nextState, [this.inverse[sequence[sequence.length-1]]]);
+                        let prevDistance = this.pruneTable.get(this.compressArr(prevState));
+                        if (prevDistance === undefined || prevDistance <= this.pruneDepth || thisDistance >= prevDistance) {
+                            yield * this.readPrun(nextState, sequence, true, Math.max(this.pruneDepth, thisDistance)); // false, address underlying problem
+                        }
+                    }
+                }
+            }   
+        }  
     }
 
     setSubgroup(generators) {
@@ -366,5 +476,84 @@ class Puzzle {
             genArray.push(this.execute(this.nullmove, this.moveStrToList(gen)));
         }
         return new Puzzle(this.cubeOri.slice(), genArray, generators, this.solved.slice());
+    }
+    
+    compressStr(str) {
+        return this.compressArr(this.execute(this.solved, this.moveStrToList(str)));
+    }
+
+    seriesMult(stateLists) {
+        let states = new Map(stateLists[0].map(x => [this.compressStr(x), x]));
+        for (let i=1; i<stateLists.length; i++) {
+            let newStates = new Map();
+            let algs = stateLists[i];
+            for (let state of states.values()) {
+                for (let alg of algs) {
+                    let newState = state + " " + alg;
+                    newStates.set(this.compressStr(newState), newState);
+                }
+            }
+            states = new Map(newStates);
+        }
+        return Array.from(states.values());
+    }
+
+    bfs(startStates, generators) {
+        let states = new Map(startStates.map(x => [this.compressStr(x), x]));
+        let newStates = new Map(startStates.map(x => [this.compressStr(x), x]));
+        let nextStates = new Map();
+        while (true) {
+            for (let state of newStates.values()) {
+                for (let gen of generators.values()) { // this can be changed; values can be omitted
+                    let prod = state + " " + gen;
+                    if (!(states.has(this.compressStr(prod)))) {
+                        nextStates.set(this.compressStr(prod), prod);
+                        states.set(this.compressStr(prod), prod);
+                    }
+                }
+            }
+            if (nextStates.size === 0) {
+                break;
+            }
+            newStates = nextStates;
+            nextStates = new Map();
+        }
+        return Array.from(states.values());
+    }
+
+    getReducedSet(states) {
+        console.log(this.adjustMovesTable);
+        let reducedStates = new Set();
+        let duplicateStates = new Set();
+        for (let state of states) {
+            let cubeStr = this.compressStr(state)
+            if (!(duplicateStates.has(cubeStr))) {
+                reducedStates.add(state);
+                for (let preAdjustment of this.adjustSequences) {
+                    for (let postAdjustment of this.adjustSequences) {
+                        duplicateStates.add(this.compressStr(this.moveListToStr(preAdjustment) + " " + state + " " + this.moveListToStr(postAdjustment)));
+                    }
+                }
+            }
+        }
+        return reducedStates;
+    }
+
+    getBatchStates(input) {
+        let parsedInput = parseBatch(input);
+        let states = [""];
+        for (let i=0; i<parsedInput.length; i++) {
+            let type = parsedInput[i][0];
+            let data = parsedInput[i][1];
+            if (type === "") {
+                states = states.map(state => state + " " + data);
+            } else if (type === "]") {
+                let algs = data.split(",");
+                states = this.seriesMult([states, algs])         
+            } else if (type === ">") {
+                states = this.bfs(states, data.split(","));
+            }
+        }
+        return this.getReducedSet(states);
     }
 }
